9f4a8e541a4083117f85436218cdad9079b99ebd
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-01-20 21:04:47 +0530

    like unlike

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createPosts } from '../controllers/postsContoller.js';
+import { createPosts, likeAndUnlikePost } from '../controllers/postsContoller.js';
+import { isAuthenticated } from '../middlewares/isAuthenticated.js';
 
 const postsRouter = express.Router();
 
 
-postsRouter.post('/posts/upload', createPosts)
+postsRouter.post('/post/upload', isAuthenticated , createPosts)
+postsRouter.get('/post/likeAndUnlike/:id', isAuthenticated , likeAndUnlikePost)
 
 export default postsRouter
