b4e4b49318075075401df6af51f1435d22a0c81b
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-01-20 21:04:01 +0530

    auth

---
+++ middlewares/isAuthenticated.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken"
+import { UserModel } from "../models/userModel.js"
+
+
+export const isAuthenticated = async (req, res, next) => {
+    try {
+        const { token } = req.cookies
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Please login."
+            })
+        }
+
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET)
+        // console.log("ddeocde user==>", decoded);
+
+        //if user is login we will store the data of user in the req.user
+        req.user = await UserModel.findById(decoded._id)
+        // console.log("req.....user==>", req.user);
+
+        next()
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: error.message
+        })
+    }
+}
