a35233126de9b22531b28600a2a29c4568a6460d
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-01-22 01:04:40 +0530

    update caption route

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createPosts, deletePost, getPostOfFollowing, likeAndUnlikePost } from '../controllers/postsContoller.js';
+import { createPosts, deletePost, getPostOfFollowing, likeAndUnlikePost, updateCaption } from '../controllers/postsContoller.js';
 import { isAuthenticated } from '../middlewares/isAuthenticated.js';
 
 const postsRouter = express.Router();
@@ -9,5 +9,6 @@ postsRouter.post('/post/upload', isAuthenticated , createPosts)
 postsRouter.get('/post/likeAndUnlike/:id', isAuthenticated , likeAndUnlikePost)
 postsRouter.delete('/post/delete/:id', isAuthenticated , deletePost)
 postsRouter.get('/post/' , isAuthenticated , getPostOfFollowing)
+postsRouter.put('/post/update/caption/:id' , isAuthenticated , updateCaption)
 
 export default postsRouter
