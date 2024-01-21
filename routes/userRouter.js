40f00c25145723f22426c8452f11d1edc1373d58
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-01-21 15:00:08 +0530

    follow user route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,11 @@
 import express  from "express";
-import { createUser, loginUser } from "../controllers/userController.js";
+import { createUser, followUser, loginUser } from "../controllers/userController.js";
+import { isAuthenticated } from '../middlewares/isAuthenticated.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/user/register' , createUser)
 userRouter.post('/user/login' , loginUser)
+userRouter.get('/user/follow/:id' , isAuthenticated , followUser)
 
 export default userRouter
