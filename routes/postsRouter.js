6081a7ccc018b71cc91cfdf43393a336fb073db6
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-01-21 15:00:31 +0530

    post of following

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createPosts, deletePost, likeAndUnlikePost } from '../controllers/postsContoller.js';
+import { createPosts, deletePost, getPostOfFollowing, likeAndUnlikePost } from '../controllers/postsContoller.js';
 import { isAuthenticated } from '../middlewares/isAuthenticated.js';
 
 const postsRouter = express.Router();
@@ -8,5 +8,6 @@ const postsRouter = express.Router();
 postsRouter.post('/post/upload', isAuthenticated , createPosts)
 postsRouter.get('/post/likeAndUnlike/:id', isAuthenticated , likeAndUnlikePost)
 postsRouter.delete('/post/delete/:id', isAuthenticated , deletePost)
+postsRouter.get('/post/' , isAuthenticated , getPostOfFollowing)
 
 export default postsRouter
