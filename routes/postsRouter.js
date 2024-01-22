6421e66c57908e0d72de486942b02f76cfbe6337
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-01-23 00:38:02 +0530

    comment on post

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createPosts, deletePost, getPostOfFollowing, likeAndUnlikePost, updateCaption } from '../controllers/postsContoller.js';
+import { commentOnPost, createPosts, deletePost, getPostOfFollowing, likeAndUnlikePost, updateCaption } from '../controllers/postsContoller.js';
 import { isAuthenticated } from '../middlewares/isAuthenticated.js';
 
 const postsRouter = express.Router();
@@ -10,5 +10,5 @@ postsRouter.get('/post/likeAndUnlike/:id', isAuthenticated , likeAndUnlikePost)
 postsRouter.delete('/post/delete/:id', isAuthenticated , deletePost)
 postsRouter.get('/post/' , isAuthenticated , getPostOfFollowing)
 postsRouter.put('/post/update/caption/:id' , isAuthenticated , updateCaption)
-
+postsRouter.put('/post/comment/:id' , isAuthenticated , commentOnPost)
 export default postsRouter
