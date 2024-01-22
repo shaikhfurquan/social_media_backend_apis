997cfff67af08a694b243e3dd15a9595812e6346
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-01-22 14:58:49 +0530

    update following model

--- models/userModel.js
+++ models/userModel.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema({
         }
     ],
 
-    followings: [
+    following: [
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User"
